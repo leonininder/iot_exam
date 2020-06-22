af6f410924c69df0ab42c77f09f0794dd6c7a70a
leonininder <leonla26@gmail.com>
2020-06-23 03:26:04 +0800

    Add files via upload

--- mongodb.py
+++ mongodb.py
@@ -9,13 +9,13 @@ table_name='iot_table'
 def constructor():
     client = pymongo.MongoClient("mongodb+srv://iot:1234qwer@cluster0-x4loq.mongodb.net/test?retryWrites=true&w=majority")
     db = client[db_name]
-    return db
+    coll = db[table_name]
+    return coll
 
 def test_connect(): 
     try:
-        db=constructor()
-        coll = db[table_name]
-        coll.count_documents({})
+        coll = constructor()
+        #coll.count_documents({})
         return 0
     except InvalidSignatureError:
         return 400
